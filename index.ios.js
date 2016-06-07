76db41a5e3da7eb5f6fead448955623bba7128ed
welfu <welfu@vip.qq.com>
2016-06-07 13:51:29 +0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+} = React;
+
+var AwesomeProject = React.createClass({
+   
+  render: function() {
+    return (
+ 	
+ 	<Image source={{uri:'test'}} style={{width: 40, height: 40}} />
+
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
